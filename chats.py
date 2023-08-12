9af1c84f85bb09d8d79c983c3e4ede2dd47bdac1
volkovskey <andrii.esf@gmail.com>
2023-08-12 18:10:45 +0300

    Fix for prev commit

--- chats.py
+++ chats.py
@@ -4,3 +4,7 @@ def writeChatsToFile():
     with open("chats.txt", "w") as file:
         for chat in DBH.GetChatIDs():
             file.write(str(chat) + "\n")
+    print("Chats written to file")
+
+if __name__ == "__main__":
+    writeChatsToFile()
